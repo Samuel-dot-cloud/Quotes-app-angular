aab4a1742476322ccf00b6208b94451aff6b2693
Samuel-dot-cloud <successam336@gmail.com>
2020-03-31 12:39:14 +0300

    work on directive

--- src/app/leader.directive.ts
+++ src/app/leader.directive.ts
@@ -12,18 +12,18 @@ constructor(private el: ElementRef) {
 
 
 let highestId;
-let vote = 0;
+let initialVote = 0;
 const allQuotes = this.quotes[0];
 const currentQuote = this.quotes[1];
 allQuotes.forEach(quote => {
-if (quote.upvote > vote) {
-vote = quote.upvote;
+if (quote.upvote > initialVote) {
+initialVote = quote.upvote;
 highestId = quote.upvote;
 }
 });
 
 if (currentQuote.upvote === highestId) {
-  this.el.nativeElement.style.backgroundColor = 'gold';
+  this.el.nativeElement.style.colors = 'gold';
   this.el.nativeElement.style.color = 'white';
  }
 
